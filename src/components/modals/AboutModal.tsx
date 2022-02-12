35b0b912ffb7040b6230134e962fcda16894663f
Marty PICCO <marty@M1.local>
2022-02-12 13:24:03 -0800

    Final about message

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -9,15 +9,21 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        This is an open source version of the word guessing game we all know and
-        love -{' '}
-        <a
-          href="https://github.com/cwackerfuss/react-wordle"
-          className="underline font-bold"
-        >
-          check out the code here
-        </a>{' '}
-      </p>
+        Freedle is the word game for those who want to be FREE to see the world they way THEY like it!
+      <br/></p>
+      <br/>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+      This SATIRICAL version of Wordle is for Freedom Lovers! Some of the important changes that true patriots are sure to love include:
+      <br/></p>
+      <br/>LOSER words that nobody knows anyway have been removed from the list of possible answers, leaving only REAL words for speaking the TRUTH
+      <br/><br/>People are saying on the internet that the letter W is really the same sex marriage of two Us, so all words containing W have been removed so people are FREE not to be made uncomfortable by THOSE words
+      <br/><br/>Some other words whose lives don't matter have also been removed
+      <br/><br/>Some proper names have been added ... you can probably guess who they are
+      <br/>
+      <br/>
+
+       <p className="text-sm text-gray-500 dark:text-gray-300">
+        Remember folks, this was done just for laughs.  You're FREE to enjoy or ignore it. </p>
     </BaseModal>
   )
 }
